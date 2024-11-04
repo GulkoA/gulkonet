31d23605ceb69f12e781bfa1a420a25007826d13
Alex Gulko <alexgulko99@gmail.com>
2024-11-04 13:18:41 -0500

    react is removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
-import react from '@astrojs/react';
+import pagefind from 'astro-pagefind';
 
 import svelte from '@astrojs/svelte';
-
-import tailwind from '@astrojs/tailwind';
 
 import sectionize from '@hbsnow/rehype-sectionize';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react(), svelte(), tailwind()],
+  integrations: [mdx(), svelte(), pagefind()],
   markdown: {
     rehypePlugins: [
       rehypeAccessibleEmojis,
